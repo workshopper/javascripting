cc8ff28d87c1069128743f6e782c65c1d2507ce5
Nick Raienko <enaqxx@gmail.com>
2014-10-17 02:46:33 +0300

    Fix greate/less than HTML entity in problem description

--- get-file.js
+++ get-file.js
@@ -5,5 +5,7 @@ var md = require('cli-md');
 module.exports = function (filepath) {
   return md(fs.readFileSync(filepath, 'utf8'))
     .replace(/&#39;/g, "'")
-    .replace(/&quot;/g, '"');
+    .replace(/&quot;/g, '"')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '<');
 };
