9948ded5db3acdd861affbcb81a8d1430d293d49
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2014-10-20 01:32:15 -0300

    added if-statement exercise

---
+++ problems/if-statement/index.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var getFile = require('../../get-file');
+var run = require('../../run-solution');
+
+exports.problem = getFile(path.join(__dirname, 'problem.md'));
+
+exports.solution = getFile(path.join(__dirname, 'solution.md'));
+
+exports.verify = function (args, cb) {
+  run(args[0], function (err, result) {
+    if (/The fruit name has more than five characters./.test(result)) cb(true);
+    else cb(false);
+  });
+};
---
+++ solutions/if-statement/index.js
@@ -0,0 +1,6 @@
+var fruit = 'orange';
+if(fruit.length > 5 ) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('The fruit name has less or equal than five characters.');
+}
