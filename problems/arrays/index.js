caf0160788138473f3936139b1b32a25b174842b
sudodoki <smd.deluzion@gmail.com>
2014-10-31 02:39:41 +0200

    Fix arrays verify step: do not fail if empty string or boolean passed in

--- problems/arrays/index.js
+++ problems/arrays/index.js
@@ -9,7 +9,7 @@ exports.solution = getFile(path.join(__dirname, 'solution.md'));
 exports.verify = function (args, cb) {
   run(args[0], function (err, result) {
     var expected = "[ 'tomato sauce', 'cheese', 'pepperoni' ]\n";
-    if (result.replace('"', "'") === expected) cb(true);
+    if (result && result.replace('"', "'") === expected) cb(true);
     else cb(false);
   });
 };
