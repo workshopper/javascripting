a0957f11186698e76a14ce8fde9ce7a93435198f
sethvincent <sethvincent@gmail.com>
2014-11-16 09:57:23 -0800

    update if-statement solution

--- solutions/if-statement/index.js
+++ solutions/if-statement/index.js
@@ -1,6 +1,6 @@
 var fruit = 'orange';
-if(fruit.length > 5 ) {
+if (fruit.length > 5) {
   console.log('The fruit name has more than five characters.');
 } else {
-  console.log('The fruit name has less or equal than five characters.');
+  console.log('The fruit name has five characters or less.');
 }
