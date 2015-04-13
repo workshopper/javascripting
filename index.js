61b4b3690b5c7b26e779c9256747770bad09765e
sethvincent <sethvincent@gmail.com>
2015-04-12 21:24:15 -0700

    add spanish translation to languages array

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var adventure = require('workshopper-adventure/adventure');
 var jsing = adventure({
     name: 'javascripting'
   , appDir: __dirname
-  , languages: ['en', 'ja', 'ko']
+  , languages: ['en', 'ja', 'ko', 'es']
 });
 
 var problems = require('./menu.json');
