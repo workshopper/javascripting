f7ca4c5201cdc0f92c21f31b291e25dcf88f9612
esalinas <efsalinas00@gmail.com>
2015-05-06 07:17:06 -0600

    Add scope lesson

--- problems/scope/solution.md
+++ problems/scope/solution.md
@@ -1,5 +1,9 @@
 ---
 
-#
+#EXCELLENT!
+
+You got it! The second function has the scope we were looking for.
+
+Run javascripting in the console to choose the next challenge.
 
 ---
---
+++ solutions/scope/index.js
@@ -0,0 +1,18 @@
+var a = 1, b = 2, c = 3;
+
+(function firstFunction(){
+	var b = 5, c = 6;
+
+	(function secondFunction(){
+		var b = 8;
+		console.log("a: "+a+", b: "+b+",c: "+c);
+		(function thirdFunction(){
+			var a = 7, c = 9;
+
+			(function fourthFunction(){
+				var a = 1, c = 8;
+
+			})();
+		})();
+	})();
+})();
